aa2feaa2a0c7f63297f6c7f9681d53f3d95f5e69
AUREAFLOW\chenf <chen.feihappy1@gmail.com>
2026-08-15 17:57:56 -0300

    update

--- backend/src/main/java/com/example/backend/config/JwtUtil.java
+++ backend/src/main/java/com/example/backend/config/JwtUtil.java
@@ -3,14 +3,19 @@ package com.example.backend.config;
 import com.example.backend.model.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "super-secret-key-change-this";
+    private final String SECRET = "super-secret-key-change-this-32bytes";
+    private final Key signingKey = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
 
     public String generateToken(User user) {
         return Jwts.builder()
@@ -20,7 +25,7 @@ public class JwtUtil {
                 .claim("lastname", user.getLastname())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(signingKey, SignatureAlgorithm.HS256)
                 .compact();
     }
 }
