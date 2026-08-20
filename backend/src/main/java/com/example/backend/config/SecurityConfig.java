1c45a2e03eaf65b5716ce83beaefb85dd16ba7b9
AUREAFLOW\chenf <chen.feihappy1@gmail.com>
2026-08-20 19:46:31 -0300

    update

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -25,7 +25,10 @@ public class SecurityConfig {
         http
             .cors(cors -> cors.configurationSource(request -> {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:4200"));
+                config.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "http://18.191.193.40"
+            ));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 config.setAllowCredentials(true);
